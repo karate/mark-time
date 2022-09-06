112578a7bd85f45000be5e4e3dd219f71be25e12
karate <karate@users.noreply.github.com>
2022-09-06 21:56:08 +0300

    Add local storage for data persistence

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,25 @@
 <script>
   import Category from './lib/Category.svelte'
   import CreateCategoryButton from './lib/CreateCategoryButton.svelte'
+
+  import {onMount} from "svelte";
+  import { writable } from 'svelte-local-storage-store'
+  import { get } from 'svelte/store'
+
   let categories = [];
+  export const preferences = writable('categories', categories);
+
+  onMount(() => {
+    // Load settings from local storage
+    categories = get(preferences);
+  });
+
+  $: {
+    // Update local storage when data change
+    if (categories.length > 0) {
+      preferences.set(categories)
+    }
+  }
 </script>
 
 <main>
